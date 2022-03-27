4a7eabd9a7c5f160fc929a83b0acd125fdc92734
FvFurquim <fvfurq@gmail.com>
2022-03-27 12:50:17 -0300

    Improve strategies

--- src/attacker.c
+++ src/attacker.c
@@ -11,6 +11,7 @@
 
 // Internal functions
 bool attacker_equals_directions(direction_t curr_dir, direction_t compar_dir);
+direction_t invert_dir_up_down_right(direction_t curr_direction);
 
 /*----------------------------------------------------------------------------*/
 /*                              PUBLIC FUNCTIONS                              */
@@ -23,43 +24,66 @@ direction_t execute_attacker_strategy(
   static direction_t direction = (direction_t) DIR_DOWN_RIGHT;
   static position_t previous_position = INVALID_POSITION;
   static int collision_count = 0;
+  static int zig_zag_count = 0;
+  static int to_avoid_i = -1;
+
+  turn++;
+
+  if(turn == 3) {
+    to_avoid_i = get_spy_position(defender_spy).i;
+  }
 
   if (equal_positions(attacker_position, previous_position)) {
+    
     collision_count++;
     
-    if(collision_count == 1) {
-      if(attacker_equals_directions(direction, (direction_t) DIR_DOWN_RIGHT))
-        direction = (direction_t) DIR_UP_RIGHT;
-
-      else if(attacker_equals_directions(direction, (direction_t) DIR_UP_RIGHT))
-        direction = (direction_t) DIR_DOWN_RIGHT;
+    if(collision_count % 4 == 1) {
+      direction = invert_dir_up_down_right(direction);
     }
 
-    else if(collision_count == 2)
+    else if(collision_count % 4 == 2)
       direction = (direction_t) DIR_RIGHT;
       
-    else if(collision_count == 3)
+    else if(collision_count % 4 == 3)
       direction = (direction_t) DIR_UP;
 
-    else if(collision_count == 4)
+    else if(collision_count % 4 == 0)
       direction = (direction_t) DIR_DOWN;
   }
 
   else {
     collision_count = 0;
+    zig_zag_count++;
 
-    if(attacker_equals_directions(direction, (direction_t) DIR_DOWN_RIGHT))
+    if(to_avoid_i != -1 && attacker_position.i >= to_avoid_i)
+      direction = (direction_t) DIR_DOWN_RIGHT;
+
+    else if(to_avoid_i != -1 && attacker_position.i <= to_avoid_i)
       direction = (direction_t) DIR_UP_RIGHT;
 
-    else if(attacker_equals_directions(direction, (direction_t) DIR_UP_RIGHT))
-      direction = (direction_t) DIR_DOWN_RIGHT;
+    else if(zig_zag_count == 2) {
+      direction = invert_dir_up_down_right(direction);
+      zig_zag_count = 0;
+    }
   }
+
+  previous_position = attacker_position;
   
   return direction;
 }
 
 bool attacker_equals_directions(direction_t curr_dir, direction_t compar_dir) {
   return curr_dir.i == compar_dir.i && curr_dir.j == compar_dir.j;
+}
+
+direction_t invert_dir_up_down_right(direction_t curr_direction) {
+  if(attacker_equals_directions(curr_direction, (direction_t) DIR_DOWN_RIGHT))
+        return (direction_t) DIR_UP_RIGHT;
+
+  else if(attacker_equals_directions(curr_direction, (direction_t) DIR_UP_RIGHT))
+    return (direction_t) DIR_DOWN_RIGHT;
+
+  return curr_direction;
 }
 
 /*----------------------------------------------------------------------------*/
--- src/defender.c
+++ src/defender.c
@@ -11,7 +11,7 @@
 #include "defender.h"
 
 // Internal functions
-bool equals_directions(direction_t curr_dir, direction_t compar_dir);
+bool defender_equals_directions(direction_t curr_dir, direction_t compar_dir);
 direction_t first_two_turns_strategy(position_t defender_position, position_t previous_position, direction_t direction);
 direction_t other_turns_strategy(position_t defender_position, position_t previous_position, direction_t direction, int center_i, int trashold);
 
@@ -43,7 +43,7 @@ direction_t execute_defender_strategy(
   return direction;
 }
 
-bool equals_directions(direction_t curr_dir, direction_t compar_dir) {
+bool defender_equals_directions(direction_t curr_dir, direction_t compar_dir) {
   return curr_dir.i == compar_dir.i && curr_dir.j == compar_dir.j;
 }
 
@@ -52,33 +52,54 @@ direction_t first_two_turns_strategy(position_t defender_position, position_t pr
   direction_t new_direction = direction;
 
   if (equal_positions(defender_position, previous_position)) {
-    if(equals_directions(direction, (direction_t) DIR_UP_LEFT))
+    if(defender_equals_directions(direction, (direction_t) DIR_UP_LEFT))
       new_direction = (direction_t) DIR_DOWN_LEFT;
 
-    else if(equals_directions(direction, (direction_t) DIR_DOWN_LEFT))
+    else if(defender_equals_directions(direction, (direction_t) DIR_DOWN_LEFT))
       new_direction = (direction_t) DIR_UP_LEFT;
   }
+
+  previous_position = defender_position;
 
   return new_direction;
 }
 
 direction_t other_turns_strategy(position_t defender_position, position_t previous_position, direction_t direction, int center_i, int trashold) {
 
+  static int collision_count = 0;
+
   direction_t new_direction = direction;
 
+  if(defender_equals_directions(direction, (direction_t) DIR_UP_LEFT))
+      new_direction = (direction_t) DIR_UP;
+
+  else if(defender_equals_directions(direction, (direction_t) DIR_DOWN_LEFT))
+    new_direction = (direction_t) DIR_DOWN;
+
   if (equal_positions(defender_position, previous_position)) {
-    if(equals_directions(direction, (direction_t) DIR_UP))
+
+    collision_count++;
+    
+    if(defender_equals_directions(direction, (direction_t) DIR_UP))
       new_direction = (direction_t) DIR_DOWN;
 
-    else if(equals_directions(direction, (direction_t) DIR_DOWN))
+    else if(defender_equals_directions(direction, (direction_t) DIR_DOWN))
       new_direction = (direction_t) DIR_UP;
+
+    if(collision_count == 2)
+      new_direction = (direction_t) DIR_RIGHT;
   }
 
-  else if(defender_position.i >= center_i + trashold)
-    new_direction = (direction_t) DIR_UP;
+  else {
 
-  else if(defender_position.i <= center_i - trashold)
-    new_direction = (direction_t) DIR_DOWN;
+    collision_count = 0;
+    
+    if(defender_position.i >= center_i + trashold)
+      new_direction = (direction_t) DIR_UP;
+  
+    if(defender_position.i <= center_i - trashold)
+      new_direction = (direction_t) DIR_DOWN;
+  }
 
   return new_direction;
 }
